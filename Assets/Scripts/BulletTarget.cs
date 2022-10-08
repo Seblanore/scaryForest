9f7245e8fd000255e0a9bf4eaa80b81b109c83f9
Seblanore <seb.lanore@gmail.com>
2022-10-08 12:44:21 +0200

    Ajout collider to bullets

--- Assets/Scripts/BulletTarget.cs
+++ Assets/Scripts/BulletTarget.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BulletTarget : NetworkBehaviour
 {
     [SerializeField] private Transform blood;
+
     private Rigidbody bulletRigidbody;
 
     public override void OnNetworkSpawn()
@@ -15,7 +16,11 @@ public class BulletTarget : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Instantiate(blood, transform.position, Quaternion.LookRotation(-transform.forward));
+        if(other.material.name == "Zombie Flesh (Instance)")
+        {
+            Instantiate(blood, transform.position, Quaternion.LookRotation(-transform.forward));
+        }
+        
         other.GetComponent<Rigidbody>().AddExplosionForce(100, transform.position, 10);
         var hitBox = other.GetComponent<HitBox>();
         if(hitBox)
